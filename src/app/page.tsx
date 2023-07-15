861512b75bd79a02c5f860b41864542ab8fc2581
Danijel Lapadatovic <l.danijel977@gmail.com>
2023-07-15 07:03:06 +0200

    Finished with custom filters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,54 @@
-import { CustomFilter, SearchBar,Hero, CarCard  } from "@/components";
-import { fetchCars } from "../../utils";
+import { fetchCars } from "../../utils"
+import { HomeProps } from "../../types";
+import { fuels, yearsOfProduction } from "../../constants";
+import { CarCard ,SearchBar, CustomFilter, Hero } from "../components";
 
-export default async function Home({searchParams}) {
+export default async function Home({ searchParams }: HomeProps) {
   const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || '',
+    manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
-    fuel: searchParams.fuel || '',
+    fuel: searchParams.fuel || "",
     limit: searchParams.limit || 10,
-    model: searchParams.model || '',
+    model: searchParams.model || "",
   });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
-    <main className="overflow-hidden">
+    <main className='overflow-hidden'>
       <Hero />
 
-      <div 
-       id="discover"
-       className="mt-12 padding-x padding-y max-width"
-      >
-        <div className="home__text-container">
-          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
-          <p className="text-gray-500"> Explore the cars you might like </p>
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
         </div>
-        <div className="home__filter">
+
+        <div className='home__filters'>
           <SearchBar />
 
-          <div className="home__filter-container">
-            <CustomFilter title='Fuel' />
-            <CustomFilter title='Year' />
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
 
         {!isDataEmpty ? (
           <section>
-            <div className="home__cars-wrapper">
-              {allCars.map((car) => (
-                <CarCard car={car}/>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
               ))}
             </div>
+
           </section>
-        )
-        :( 
-          <div className="home__error-container">
-            <h2 className="text-black text-xl font-bold">Ooops, no result</h2>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
-
       </div>
     </main>
-  )
+  );
 }
--- utils/index.ts
+++ utils/index.ts
@@ -16,20 +16,6 @@ export async function fetchCars(filters: FilterProps) {
     return result;
 }
 
-export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-    const url = new URL("https://cdn.imagin.studio/getimage");
-    const { make, model, year } = car;
-  
-    url.searchParams.append('customer', process.env.IMAGIN_STUDIO_KEY || '');
-    url.searchParams.append('make', make);
-    url.searchParams.append('modelFamily', model.split(" ")[0]);
-    url.searchParams.append('zoomType', 'fullscreen');
-    url.searchParams.append('modelYear', `${year}`);
-    // url.searchParams.append('zoomLevel', zoomLevel);
-    url.searchParams.append('angle', `${angle}`);
-    
-    return `${url}`;
-  } 
 
 export const calculateCarRent = (city_mpg:number, year: number) => {
     const basePricePerDay = 50;  // Base rental price per day in dollars
@@ -44,4 +30,29 @@ export const calculateCarRent = (city_mpg:number, year: number) => {
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
     return rentalRatePerDay.toFixed(0);
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+  
+    url.searchParams.append('customer', process.env.IMAGIN_STUDIO_KEY || '');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+    
+    return `${url}`;
+} 
+
+
+export const updateSearchParams = (type:string, value:string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(type, value);
+    
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+    
+    return newPathname;
 }
